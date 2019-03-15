43a87dff4fccb5d4d7baff4aa9832a96321a361f
Excalibur <excalibur44@outlook.com>
2019-03-15 09:34:16 +0800

    Add cloudsync.py (Finish!)

---
+++ cloudsync/cloudsync.py
@@ -0,0 +1,42 @@
+import sys
+
+from cfs import CloudFileSystem
+from synchronization_single_cloud import SynchronizationSingleCloud
+
+
+def main():
+    if len(sys.argv) == 3 and sys.argv[1] in ['-s', '--sync'] and sys.argv[2] in ['tencent', 'ali']:
+        print('Start sync with {csp}...'.format(csp=sys.argv[2]))
+        start_sync(csp=sys.argv[2])
+    else:
+        print_help()
+
+
+def start_sync(csp):
+    """
+    根据 cosp 的名字启动对应的同步程序
+    :param csp: COS Service Provider
+    :return: None
+    """
+    if csp == 'tencent':
+        cfs = CloudFileSystem('tencent')
+    elif csp == 'ali':
+        cfs = CloudFileSystem('ali')
+    else:
+        print("Unknown COS Service Provider!")
+        return
+
+    SynchronizationSingleCloud(cfs).start()
+
+
+def print_help():
+    print('''\
+usage: python3 cloudsync.py [OPTION] ...
+options:
+  -s, --sync {tencent|ali}  指定数据同步到选定云
+  -h, --help                输出此帮助文档
+''')
+
+
+if __name__ == '__main__':
+    main()
