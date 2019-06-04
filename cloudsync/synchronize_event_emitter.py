f00d3caf86f20ccb20209d357f38dc393d4986ab
Excalibur <excalibur44@outlook.com>
2019-06-04 09:40:13 +0800

    因不用传递元信息树这参数，精简了传递处理器参数的操作

--- cloudsync/synchronize_event_emitter.py
+++ cloudsync/synchronize_event_emitter.py
@@ -1,6 +1,3 @@
-from cos_config import ops_constants
-
-
 class SynchronizeEventEmitter:
     """
     任务参数管理，中介对象
@@ -25,36 +22,12 @@ class SynchronizeEventEmitter:
         for observer in self._observers:
             observer.update(self)
 
-    def set_data(self, obj, task_index, *args):
+    def set_data(self, task_index, *args):
         self.task_index = task_index
-        # 删除文件、创建目录、下载文件、上传文件
-        if task_index in [ops_constants['UPLOAD_FILE'],
-                          ops_constants['DOWNLOAD_FILE'],
-                          ops_constants['CREATE_CLOUD_FOLDER'],
-                          ops_constants['RENAME_CLOUD_FOLDER'],
-                          ops_constants['RENAME_LOCAL_FOLDER'],
-                          ops_constants['CREATE_LOCAL_FOLDER']
-                          ]:
-            self.directory_list = obj
-            self.from_path = args[0]
+        self.from_path = args[0]
+        try:
             self.to_path = args[1]
-        if task_index in [ops_constants['DELETE_CLOUD_FILE'],
-                          ops_constants['DELETE_CLOUD_FOLDER']]:
-            self.directory_list = obj
-            self.from_path = args[0]
-
-        # 本地删除文件、目录和创建目录
-        if task_index in [ops_constants['DELETE_LOCAL_FOLDER'],
-                          ops_constants['DELETE_LOCAL_FILE']]:
-            self.directory_list = obj
-            self.from_path = args[0]
-
-        # 重命名、更新文件
-        if task_index in [ops_constants['RENAME_CLOUD_FILE'],
-                          ops_constants['RENAME_LOCAL_FILE'],
-                          ops_constants['UPDATE_CLOUD_FILE']]:
-            self.stat_file = obj
-            self.from_path = args[0]
-            self.to_path = args[1]
+        except IndexError:
+            self.to_path = ''
 
         self.notify()
