4c489e0197cdb876aa5a707daf08871a4b934476
Excalibur <excalibur44@outlook.com>
2019-07-14 10:51:59 +0800

    Update(utils.py): 删除无用的(整个项目未调用过的)函数

--- cloudsync/utils.py
+++ cloudsync/utils.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import hashlib
-from catalog import Catalog, initialize_metatree_local, initialize_metatree_cloud
+from catalog import Catalog
 
 # 加密算法 (可选 sha1 | sha256 | sha512)
 hash_type = "sha256"
@@ -19,6 +19,11 @@ def get_local_file_hash(file_path):
         return getattr(hashlib, hash_type)(f.read()).hexdigest()
 
 
+def get_cloud_file_hash(cloud_path, cfs):
+    cfs.download(cloud_path, temp_file_path)
+    return get_local_file_hash(temp_file_path)
+
+
 def get_buffer_hash(buffer: bytes):
     """
     计算字节数组的摘要
@@ -26,11 +31,6 @@ def get_buffer_hash(buffer: bytes):
     :return: 摘要值
     """
     return getattr(hashlib, hash_type)(buffer).hexdigest()
-
-
-def get_cloud_file_hash(cloud_path, cfs):
-    cfs.download(cloud_path, temp_file_path)
-    return get_local_file_hash(temp_file_path)
 
 
 def get_entire_local_directory_hash(directory):
@@ -55,55 +55,3 @@ def get_entire_cloud_directory_hash(directory, cfs):
             files_hash_value += child.hash_value
     directory.hash_value = get_buffer_hash(files_hash_value.encode())
     return directory.hash_value
-
-
-def get_entire_directory(directory):
-    files_hash_value = ''
-    for child in directory.child:
-        if child.file_type == Catalog.IS_FOLDER:
-            files_hash_value += get_entire_directory(child)
-        else:
-            files_hash_value += child.hash_value
-    directory.hash_value = get_buffer_hash(files_hash_value.encode())
-    return directory.hash_value
-
-
-def get_entire_directory_hash_by_local_path(local_path):
-    root = initialize_metatree_local(local_path)
-    files_hash_value = ''
-    for it in root.child:
-        if it.file_type == Catalog.IS_FOLDER:
-            it.hash_value = get_entire_directory_hash_by_local_path(local_path + it.filename)
-            files_hash_value += it.hash_value
-        elif it.file_type == Catalog.IS_FILE:
-            it.hash_value = get_local_file_hash(local_path + it.filename)
-            files_hash_value += it.hash_value
-        else:
-            print("Unknown file type!")
-    # 计算目录摘要值
-    return get_buffer_hash(files_hash_value.encode())
-
-
-def get_entire_directory_hash_by_cloud_path(cloud_path, cfs):
-    root = initialize_metatree_cloud(cloud_path, cfs)
-    files_hash_value = ''
-    for it in root.child:
-        if it.file_type == Catalog.IS_FOLDER:
-            it.hash_value = get_entire_directory_hash_by_cloud_path(cloud_path + it.filename, cfs)
-            files_hash_value += it.hash_value
-        elif it.file_type == Catalog.IS_FILE:
-            it.hash_value = get_cloud_file_hash(cloud_path + it.filename, cfs)
-            files_hash_value += it.hash_value
-        else:
-            print("Unknown file type!")
-    # 计算目录摘要值
-    return get_buffer_hash(files_hash_value.encode())
-
-
-def str2hex(s):
-    """
-    将字符串转换成十六进制
-    :param s: 输入串
-    :return: 十六进制字符串
-    """
-    return ''.join([hex(ord(i))[2:] for i in s])
